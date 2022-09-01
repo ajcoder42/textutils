d7ecea74287aef104a78cf4184a01e7456f8baef
ajcoder42 <84202466+ajcoder42@users.noreply.github.com>
2022-09-02 00:26:50 +0530

    Updates

---
+++ precache-manifest.9ce3f5af34878988da9a0e22a36de884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "270006c0c1e309975324",
+    "url": "/textutils/static/css/main.3e68ba52.chunk.css"
+  },
+  {
+    "revision": "270006c0c1e309975324",
+    "url": "/textutils/static/js/main.270006c0.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "424e40fc9afa8822750bb5b75ddf8c83",
+    "url": "/textutils/index.html"
+  }
+];
